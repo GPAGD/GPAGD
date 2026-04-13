d7c5d05ed560fda46dff55369837dc42c39fff2f
GPAGD <gpagd@outlook.com>
2026-04-13 19:38:50 +0200

    Update __init__.py

--- gpagd/__init__.py
+++ gpagd/__init__.py
@@ -1,3 +1,10 @@
-from .optimizer import GeometricPhysicsGD
+from .optimizers import GeometricPhysicsGD, PhysicsAwareGD
+from .utils import PCAManifoldProjector, local_entropy_1d, local_entropy_grid
 
-__all__ = ["GeometricPhysicsGD"]
+__all__ = [
+    "GeometricPhysicsGD",
+    "PhysicsAwareGD",
+    "PCAManifoldProjector",
+    "local_entropy_1d",
+    "local_entropy_grid",
+]
