d1df33fa3b259544064d70ac2c52ffe86b6163da
GPAGD <gpagd@outlook.com>
2026-04-13 19:57:22 +0200

    Update __init__.py

--- benchmarks/__init__.py
+++ benchmarks/__init__.py
@@ -3,4 +3,9 @@ from .burgers import Burgers1D_Fixed
 from .darcy import Darcy2D
 from .taylor_green import TaylorGreen2D
 
+__all__ = ["Poisson1D", "Burgers1D_Fixed", "Darcy2D", "TaylorGreen2D"]from .poisson import Poisson1D
+from .burgers import Burgers1D_Fixed
+from .darcy import Darcy2D
+from .taylor_green import TaylorGreen2D
+
 __all__ = ["Poisson1D", "Burgers1D_Fixed", "Darcy2D", "TaylorGreen2D"]
